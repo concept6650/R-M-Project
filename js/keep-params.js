85b636e07479b27c8e06f9bb1803caf34788c4b0
concept6650 <240935697+concept6650@users.noreply.github.com>
2025-11-12 19:37:00 +0000

    add countries

---
+++ js/keep-params.js
@@ -0,0 +1,16 @@
+// fixes intellij reload on save
+(function () {
+    document.addEventListener("DOMContentLoaded", () => {
+        let params = window.location.search;
+
+        document.querySelectorAll("a[href]").forEach(link => {
+            const url = new URL(link.href);
+
+            if (url.origin !== window.location.origin) return; // don't affect external links
+
+            params = new URLSearchParams(params);
+            url.search = params.toString();
+            link.href = url.toString();
+        });
+    });
+})();
