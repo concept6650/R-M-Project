9d42ef1e247623675665e7e457533006975767ce
sam-xec <samishaikh20041507@gmail.com>
2025-11-27 16:09:06 +0000

    removed features

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
-const parentDiv = document.querySelector(".carousel");
-let lastIndex = parentDiv.children.length - 1;
-
-let temp; 
-setInterval(() => {
-   
-   let insertRef = parentDiv.children[0];
-   temp = parentDiv.children[lastIndex];
-   parentDiv.removeChild(temp);
-   parentDiv.insertBefore(temp, insertRef);
-    parentDiv.children[3].classList.remove("main-item");
-     parentDiv.children[2].classList.add("main-item");
-
-   
-},3500);
-
 // Dropdown menu
 
 const dropdownMenu = document.querySelector(".dropdown-menu");
